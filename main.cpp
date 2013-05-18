a4bbcf996bb7a0e67f6380b747f5f8bf20944b06
Diana Mincu <diana.p.mincu@gmail.com>
2013-05-18 13:57:06 +0300

    Reverted changes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <windows.h>
 #include <MMSystem.h>
 
-#include <gl\gl.h>
-#include "glut.h"
+#include <gl\gl.h>								
+#include "glut.h"							
 
 #include "main.h"
 #include "engine.h"
@@ -13,20 +13,20 @@
 #pragma comment(lib, "glu32.lib")
 #pragma comment(lib, "GLAUX.lib")
 
-TCHAR szTitle[MAX_LOADSTRING];
-TCHAR szWindowClass[MAX_LOADSTRING];
+TCHAR szTitle[MAX_LOADSTRING];                          
+TCHAR szWindowClass[MAX_LOADSTRING];                    
 
-HGLRC           hRC     = NULL;
-HDC             hDC     = NULL;
-HWND            hWnd    = NULL;
-HINSTANCE       hInst   = NULL;
+HGLRC           hRC     = NULL;					        
+HDC             hDC     = NULL;					        
+HWND            hWnd    = NULL;			        		
+HINSTANCE       hInst   = NULL;	        				
 
-BOOL	bActive     = TRUE;
-BOOL	bFullscreen = TRUE;
+BOOL	bActive     = TRUE;							    
+BOOL	bFullscreen = TRUE;		
 BOOL	fsP			= FALSE;
 BOOL	fog			= FALSE;
 
-BOOL	bArrKeys[256];
+BOOL	bArrKeys[256];								    
 BOOL	lp = FALSE;
 BOOL    xp = FALSE;
 BOOL    sp = FALSE;
@@ -34,52 +34,53 @@ BOOL    np = FALSE;
 BOOL    pp = FALSE;
 
 
-BOOL	bUseLighting    = TRUE;
+BOOL	bUseLighting    = TRUE;						
 BOOL    bCullFaces      = FALSE;
 BOOL    bDrawNormals    = FALSE;
 BOOL    bNoClipping     = FALSE;
 
-LRESULT CALLBACK WndProc(HWND	hWnd,
-		        		 UINT	uMsg,
-				         WPARAM	wParam,
-				         LPARAM	lParam)
+LRESULT CALLBACK WndProc(HWND	hWnd,					
+		        		 UINT	uMsg,					
+				         WPARAM	wParam,					
+				         LPARAM	lParam)					
 {
-    switch (uMsg) {
-        case WM_ACTIVATE:
+    switch (uMsg) {	        							
+        case WM_ACTIVATE:			        			
 		    {
-			    if (!HIWORD(wParam)) {
-			        bActive = TRUE;
+			    if (!HIWORD(wParam)) {		        	
+			        bActive = TRUE;					    
 			    } else {
-				    bActive = FALSE;
+				    bActive = FALSE;					
 			    }
-			return 0;
+			return 0;           						
 		    }
-		case WM_SYSCOMMAND:
+		case WM_SYSCOMMAND:			        			
     		{
-	    		switch (wParam) {
-		    	   	case SC_SCREENSAVE:
-    				case SC_MONITORPOWER:
-				        return 0;
+	    		switch (wParam) {   					
+		    	   	case SC_SCREENSAVE:		    		
+    				case SC_MONITORPOWER:				
+				        return 0;	    				
 			    }
-			    break;
+			    break;					        		
 		    }
-		case WM_CLOSE:
+		case WM_CLOSE:		        					
 		    {
-    			PostQuitMessage(0);
+    			PostQuitMessage(0);		    			
+			    return 0;   	    					
+		    }
+        case WM_KEYDOWN:					        	
+		    {
+    			bArrKeys[wParam] = TRUE;				
 			    return 0;
 		    }
-        case WM_KEYDOWN:
-		    {
-    			bArrKeys[wParam] = TRUE;
+        case WM_KEYUP:				        			
+    		{
+			    bArrKeys[wParam] = FALSE;	    		
 			    return 0;
 		    }
-        case WM_KEYUP:
-    		{
-			    bArrKeys[wParam] = FALSE;
-		    }
-        case WM_SIZE:
+        case WM_SIZE:						        	
 		    {
-    			resizeGLScene(LOWORD(lParam), HIWORD(lParam));
+    			resizeGLScene(LOWORD(lParam), HIWORD(lParam));	
 			    return 0;
 		    }
     }
@@ -161,53 +162,46 @@ void dispatchKeys() {
 
 int WINAPI WinMain(HINSTANCE	hInstance,
 		           HINSTANCE	hPrevInstance,
-			       LPSTR		lpCmdLine,
-			       int	    	nCmdShow)
+			       LPSTR		lpCmdLine,				
+			       int	    	nCmdShow)				
 {
  	MSG msg;
- 	BOOL bDone = FALSE;
+ 	BOOL bDone = FALSE;                               
 	
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	LoadString(hInstance, IDC_GLMAZE3, szWindowClass, MAX_LOADSTRING);
 	
-	bFullscreen = FALSE;
-	bool startGame = FALSE;
-
-	startGame = TRUE;
-
-	if (startGame){
-		// Create Our OpenGL Window
-		if (!createGLWindow(szTitle, 640, 480, 32, bFullscreen)) {
-			return 0;
-		}
-
-		PlaySound(TEXT("ObstacleCourse.wav"), NULL, SND_ASYNC|SND_FILENAME|SND_LOOP);
-	    
-		while (!bDone) {
-			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-				if (msg.message == WM_QUIT) {
-					bDone = TRUE;
-				} else {
-					TranslateMessage(&msg);
-					DispatchMessage(&msg);
-				}
-			} else {
-				// Draw the scene.  Watch for ESC key and quit messages
-				if (bActive) {
-					if (bArrKeys[VK_ESCAPE]) {
-						bDone = TRUE;
-					} else {
-						drawGLScene();
-						SwapBuffers(hDC);
-						dispatchKeys();
-					}
-				}
-			}
-		}
+	bFullscreen = FALSE;	    					
+    // Create Our OpenGL Window
+	if (!createGLWindow(szTitle, 640, 480, 32, bFullscreen)) {
+		return 0;           							
 	}
 
+	PlaySound(TEXT("ObstacleCourse.wav"), NULL, SND_ASYNC|SND_FILENAME|SND_LOOP);
+    
+    while (!bDone) {     								
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+		    if (msg.message == WM_QUIT) {				
+			    bDone = TRUE;
+			} else {						        	
+			    TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		} else {        								
+		    // Draw the scene.  Watch for ESC key and quit messages
+			if (bActive) {        						
+				if (bArrKeys[VK_ESCAPE]) {				
+				    bDone = TRUE;		        		
+				} else {            					
+				    drawGLScene();		        		
+					SwapBuffers(hDC);			        
+					dispatchKeys();
+                }
+            }
+        }
+    }
+
 	// Shutdown
-	destroyGLWindow();
-	
-	return (msg.wParam);
+	destroyGLWindow();					    		
+	return (msg.wParam);					    		
 }
