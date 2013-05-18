cfeb10f8199f361743fcb79da2358b1d29d6a98b
Diana Mincu <diana.p.mincu@gmail.com>
2013-05-18 17:19:04 +0300

    Added button functionality to change window

--- main.cpp
+++ main.cpp
@@ -172,10 +172,9 @@ void dispatchKeys_userinput(){
 int drawUserInputBox(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
+	glClearColor(0, 0, 0, 1);
 
 	glPushMatrix();
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1);
 
 	glPopMatrix();
 
